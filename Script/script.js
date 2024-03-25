38fe5c19669a35a6930e3ea753bad8bde01c1379
alenasmartp <150282056+alenasmartp@users.noreply.github.com>
2024-03-25 19:38:23 +0300

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,102 @@
+// Task 1
+let d=7;
+console.log(d);
+console.log(d * d);
+console.log((d*d)*d);
+
+// Task 2
+// let num1 = prompt("Введите первое число");
+// let num2 = prompt("Введите второе число");
+// if (num1 > num2) {
+//     console.log(num1+num2);
+// }
+// if (num1==num2) {
+// console.log("Числа одинаковые");
+// }
+// else {
+//     console.log(num1*num2);
+// }
+
+
+// Task 3
+// let num = prompt("Введите число");
+// if (num > 0) {
+//     console.log((num * num) * num);
+// }
+
+// Task 4
+// let kids = prompt("Сколько учеников в классе?");
+// let chairs = prompt("Сколько стульев в кабинете?");
+// if (kids < chairs) {
+//     console.log("Стульев хватает");
+// }
+// else {
+//     console.log("Стульев не хватает");
+// }
+
+// Task 5
+// let array = [-2, 3, 0, 7, -5, 28, -90];
+// for (let i = 0; i < array.length; i++){
+//     if (array[i] >= 0) {
+//         console.log(array[i]);
+//     }
+// }
+
+// Task 6
+// let array = [4, 7, 11, 15, 21, 33];
+// let array1 = [];
+// for (let i = 0; i < array.length; i++){
+//   if (array[i] >= 15) {
+//     array1.push(array[i]);
+//   }
+// }
+// console.log(array1);
+
+// Task 7
+// let array = [4, 7, 11, 15, 18, 21, 33, 38];
+// let array1 = [];
+// for (let i = 0; i < array.length; i++){
+//   if (array[i]%2 != 0) {
+//     array1.push(array[i]);
+//   }
+// }
+// console.log(array1);
+
+// Task 8
+// let array = [2, 6, 8, 9, 12];
+// let array1 = [];
+// for (let i = 0; i < array.length; i++){
+//   if (array[i] % 3 == 0) {
+//     array1.push(array[i]);
+//   }
+// }
+// console.log(array1);
+    
+// Task 9
+// let array = [4, 7, 11, 15, 18, 21, 33, 38];
+// let sum = 0;
+// for (let i = 0; i < array.length; i++){
+//   if (array[i]%2 != 0) {
+//     sum = sum + array[i];
+//   }
+// }
+// console.log(sum);
+
+// Task 10
+// let array = [4, 7, 11, 15, 18, 21, 33, 38];
+// let sum1 = 0;
+// let sum2 = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 != 0) {
+//     sum1 = sum1 + array[i];
+//     }
+//   else {
+//       sum2 = sum2 + array[i];
+//     }
+// }
+// if (sum1 > sum2) {
+//     console.log(sum1 - sum2);
+// }
+// else {
+//     console.log(sum2 - sum1);
+// }
